322dbe94c3a9a8a12702c65315f4d5eeee6d6c28
CHANT-MAC <cca_company@naver.com>
2016-04-18 15:46:28 +0900

    develop todo

---
+++ karma.conf.js
@@ -0,0 +1,9 @@
+// karma.conf.js
+module.exports = function(config) {
+    config.set({
+        frameworks: ["mocha", "chai"],
+        files: ["js/todos.js", "test/todo.test.js"],
+        browsers: ["PhantomJS"],
+        singleRun: true
+    });
+};
